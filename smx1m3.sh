8319da13d52c7aa3edad5c288e8b08d6adefd911
coordinacioJDA <40385094+coordinacioJDA@users.noreply.github.com>
2018-06-29 10:36:58 +0000

    Update smx1m3.sh

--- smx1m3.sh
+++ smx1m3.sh
@@ -1,6 +1,6 @@
 apt-add-repository -y ppa:sylvain-pineau/kazam
 sudo add-apt-repository ppa:thomas-schiex/blender
 apt -y update
-apt -y install audacity inkscape openshot vokoscreen kazam recordmydesktop guvcview cheese kexi kdenlive blender
+apt -y install audacity inkscape openshot vokoscreen kazam recordmydesktop guvcview cheese kexi kdenlive blender libreoffice-base
 #Extensió de Thunderbird https://addons.mozilla.org/thunderbird/downloads/file/633791/lightning-5.4-sm+tb-linux.xpi?src=api . Mirar si alumnes poden instal·lar-ho.
 
